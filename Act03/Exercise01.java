38c8cdfdb304c09f44f2ffe661ea02f497a71d45
Sinya47 <sinya4741@gmail.com>
2023-03-29 21:34:24 +0900

    Sin

--- Act03/Exercise01.java
+++ Act03/Exercise01.java
@@ -4,5 +4,6 @@ public class Exercise01 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello World");
+        System.out.println("Helllo Sin");
     }
 }
